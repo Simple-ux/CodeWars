f7b3d7636e5cf4796da830ad54b7bcad1a0d330f
Simple-ux <kirill_99-99@mail.ru>
2022-08-11 21:52:42 +0300

    pig_it

---
+++ pig_it.py
@@ -0,0 +1,12 @@
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+# Leave punctuation marks untouched.
+
+# Examples
+# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+# pig_it('Hello world !')     # elloHay orldway !
+
+def pig_it(text):
+    splited = text.split()
+    print(" ".join([word[1:] + word[0] + "ay" if word[0].isalpha() else word[0]  for word in splited]))
+
+pig_it('O tempora o mores !')
