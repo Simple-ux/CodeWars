d9d2db27b6ca6911b4b836c76862cdb20bdfac73
Kirill Karasev <61629596+Simple-ux@users.noreply.github.com>
2022-08-11 10:50:32 +0300

    Update Find_The_Parity_Outlier.py

--- Find_The_Parity_Outlier.py
+++ Find_The_Parity_Outlier.py
@@ -12,6 +12,6 @@ def find_outlier(integers):
         return res_2[0]
     
 # Tests
-# print(find_outlier([2, 4, 6, 8, 10, 3]), 3)
-# print(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36]), 11)
-# print(find_outlier([160, 3, 1719, 19, 11, 13, -21]), 160)
+# print(find_outlier([2, 4, 6, 8, 10, 3]))
+# print(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36]))
+# print(find_outlier([160, 3, 1719, 19, 11, 13, -21]))
