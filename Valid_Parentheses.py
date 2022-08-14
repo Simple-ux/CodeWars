4b0ad8e96f451828d62382a36cff0917b6fce325
Simple-ux <kirill_99-99@mail.ru>
2022-08-14 18:15:50 +0300

    valid

---
+++ Valid_Parentheses.py
@@ -0,0 +1,21 @@
+# Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+# The function should return true if the string is valid, and false if it's invalid.
+
+# Examples
+# "()"              =>  true
+# ")(()))"          =>  false
+# "("               =>  false
+# "(())((()())())"  =>  true
+
+def valid_parentheses(string):
+    arr = ""
+    for i in string:
+        if (i == ")"):
+            arr += i + ","
+        elif (i == "("):
+            arr += i
+    try:  
+        if exec(arr) == None:
+            return True
+    except:
+        return False
