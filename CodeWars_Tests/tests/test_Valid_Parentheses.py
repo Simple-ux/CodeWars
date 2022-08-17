624d60962dc438319f42db90f4227e1fddd90383
Simple-ux <kirill_99-99@mail.ru>
2022-08-17 22:18:59 +0300

    tests

--- CodeWars_Tests/tests/test_Valid_Parentheses.py
+++ CodeWars_Tests/tests/test_Valid_Parentheses.py
@@ -4,16 +4,29 @@ sys.path.append("../../")
 from CodeWars.Tasks_Solutions.Valid_Parentheses import valid_parentheses
 
 def test_valid_1():
-    assert valid_parentheses("hi(ds))(") == False
-
-def test_valid_2():
     assert valid_parentheses("()(()())") == True
 
+def test_valid_2():
+    assert valid_parentheses("()") == True
+
 def test_valid_3():
-    assert valid_parentheses("9(()(9)))()(") == False
+    assert valid_parentheses("(())((()())())") == True
 
 def test_valid_4():
-    assert valid_parentheses(")") == False
+    assert valid_parentheses("hi(ds))(") == False
 
 def test_valid_5():
+    assert valid_parentheses("9(()(9)))()(") == False
+
+def test_valid_6():
+    assert valid_parentheses(")") == False
+
+def test_valid_7():
     assert valid_parentheses("(") == False
+
+def test_valid_8():
+    assert valid_parentheses(")(()))") == False
+
+def test_valid_9():
+    assert valid_parentheses("(") == False
+
