2e5ea864824e61c05483f459bd16cd538727bb42
Simple-ux <kirill_99-99@mail.ru>
2022-08-19 21:20:29 +0300

    Algorythms, strings

---
+++ CodeWars_Tests/tests/test_Strip_Comments.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("../../")
+
+from CodeWars.Tasks_Solutions.Strip_Comments import strip_comments
+
+def test_1():
+    assert strip_comments('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!']) == 'apples, pears\ngrapes\nbananas'
+
+def test_2():
+    assert strip_comments('a #b\nc\nd $e f g', ['#', '$']) == 'a\nc\nd'
+
+def test_3():
+    assert strip_comments('Jonny B $ Good\ntrue\n water$mellon\n', ['#', '$']) == "Jonny B\ntrue\n water\n"
